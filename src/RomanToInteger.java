13f9df00ef02b59262f60b40a5151663fee68455
Isabel Oliveira <isabel.01oa@outlook.com>
2023-02-03 15:56:59 -0300

    added an array in the code

--- src/RomanToInteger.java
+++ src/RomanToInteger.java
@@ -6,31 +6,42 @@ public class RomanToInteger {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Input: ");
-        String roman = sc.nextLine();
+        String roman = sc.next();
+
+        int[] number = new int[roman.length()];
 
         int count = 0;
 
         for(int i = 0; i < roman.length(); i++) {
             if(roman.charAt(i) == 'I') {
-                count += 1;
+                number[i] = 1;
             }
             else if(roman.charAt(i) == 'V') {
-                count += 5;
+                number[i] = 5;
             }
             else if(roman.charAt(i) == 'X') {
-                count += 10;
+                number[i] = 10;
             }
             else if(roman.charAt(i) == 'L') {
-                count += 50;
+                number[i] = 50;
             }
             else if(roman.charAt(i) == 'C') {
-                count += 100;
+                number[i] = 100;
             }
             else if(roman.charAt(i) == 'D') {
-                count += 500;
+                number[i] = 500;
             }
             else if(roman.charAt(i) == 'M') {
-                count += 1000;
+                number[i] = 1000;
+            }
+        }
+
+        for(int i = 0; i < roman.length(); i++) {
+            if(number[i] > number[i + 1]) {
+                count = number[i] + number[i + 1];
+            }
+            else {
+                count = (number[i + 1] - number[i]);
             }
         }
 
