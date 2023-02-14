35e78c8b2285d1d510fbfe23b73671d3a2f23ed5
Isabel Oliveira <isabel.01oa@outlook.com>
2023-02-14 14:59:32 -0300

    created method

--- src/RomanToInteger.java
+++ src/RomanToInteger.java
@@ -6,13 +6,13 @@ public class RomanToInteger {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Input: ");
-        String roman = sc.next();
+        String roman = sc.next().toUpperCase();
 
         int[] number = new int[roman.length()];
 
         int count = 0;
 
-        for(int i = 0; i < roman.length(); i++) {
+        for(int i = roman.length(); i == 0; i--) {
             if(roman.charAt(i) == 'I') {
                 number[i] = 1;
             }
@@ -34,39 +34,21 @@ public class RomanToInteger {
             else if(roman.charAt(i) == 'M') {
                 number[i] = 1000;
             }
-        }
-
-        if(number[0] <= number[1]) {
-            for(int i = 0; i < roman.length(); i++) {
-                for(int j = 0; j < roman.length(); j++) {
-                    if(number[i] >= number[j] && j != i) {
-                        count += number[i];
-                        break;
-                    }
-                    else if(number[i] <= number[j] && j != i){
-                        count -= number[i];
-                        break;
-                    }
-                }
-            }
-        }
-        else {
-            for(int i = 0; i < roman.length(); i++) {
-                for(int j = 0; j < roman.length(); j++) {
-                    if(number[i] >= number[j] && j != i) {
-                        count += number[i];
-                        break;
-                    }
-                    else if(number[i] <= number[j] && j != i){
-                        count += number[i];
-                        break;
-                    }
-                }
-            }
+            count += sumRoman(number[i]);
         }
 
         System.out.println("Output: " + count);
 
         sc.close();
+    }
+    private static int sumRoman(int number) {
+        int sum = 0;
+        if(number > sum) {
+            sum += number;
+        }
+        else {
+            sum -= number;
+        }
+        return sum;
     }
 }
